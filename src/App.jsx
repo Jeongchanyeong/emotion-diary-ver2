2e1aecaee316e78b747de05a301fa04f2fd1f22f
정찬영 <q135794268020@gmail.com>
2024-03-14 23:52:58 +0900

    Feat/ edit page useParams, mokData 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,57 @@
 import './App.css';
-import { Routes, Route, Link, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
 import NotFound from './pages/NotFound';
-import { getEmotionImage } from './util/get-emotion-img';
+
 import Button from './components/common/Button';
 import Header from './components/common/Header';
+import { useReducer } from 'react';
+
+const mokData = [
+  {
+    id: 1,
+    createdDate: new Date().getTime(),
+    emotionId: 1,
+    content: '1번 일기 내용',
+  },
+  {
+    id: 2,
+    createdDate: new Date().getTime(),
+    emotionId: 2,
+    content: '2번 일기 내용',
+  },
+  {
+    id: 3,
+    createdDate: new Date().getTime(),
+    emotionId: 3,
+    content: '3번 일기 내용',
+  },
+];
+
+const reducer = (state, action) => {
+  return state;
+};
+
+// 하나의 일기 내부에는 날짜, 감정, 일기 내용이 필요함
 
 function App() {
-  // 동적 경로: 동적인 데이터를 포함하고 있는 경로
-  // ex) ~/product/1  ~/product/2  ~/product/3
-
-  // URL Parameter: 아이템의 id 등의 변경되지 않는 값을 주소로 명시하기 위해 사용
-  // Query String: 검색어 등의 자주 변경되는 값을 주소로 명시하기 위해 사용
-
-  // useNavigate를 사용하여 페이지 이동
-  const nav = useNavigate();
-
+  const [data, dispatch] = useReducer(reducer, mokData);
+  console.log(dispatch);
+  console.log(data);
+  console.log(mokData);
+  console.log(data === mokData);
+  console.log(reducer);
   return (
     <>
-      <Header
-        title={'Header'}
-        leftChild={<Button text={'<'} />}
-        rightChild={<Button text={'>'} />}
-      />
-
-      <Button
-        type={'DEFAULT'}
-        text={123}
-        onClick={() => console.log('123123')}
-      />
-      <Button
-        type={'POSITIVE'}
-        text={123}
-        onClick={() => console.log('123123')}
-      />
-      <Button
-        type={'NEGATIVE'}
-        text={123}
-        onClick={() => console.log('123123')}
-      />
-
       <Routes>
         {/* Route를 통한 경로 설정 */}
         <Route path="/" element={<Home />} />
         <Route path="/new" element={<New />} />
         <Route path="/diary/:id" element={<Diary />} />
-        <Route path="/edit" element={<Edit />} />
+        <Route path="/edit/:id" element={<Edit />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </>
