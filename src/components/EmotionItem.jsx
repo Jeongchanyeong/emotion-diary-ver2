fe76f606464f41c6d6c9c8b866555190422b7b9d
정찬영 <q135794268020@gmail.com>
2024-03-17 00:03:15 +0900

    Feat/ NewPage UI 구현

--- src/components/EmotionItem.jsx
+++ src/components/EmotionItem.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import './EmotionItem.css';
-
-const EmotionItem = () => {
-  return <div></div>;
+import { getEmotionImage } from '../util/get-emotion-img';
+const EmotionItem = ({ emotionId, emotionName, isSelected }) => {
+  return (
+    <div
+      className={`EmotionItem ${
+        isSelected ? `EmotionItem_on_${emotionId}` : ''
+      }`}
+    >
+      <img className="emotion_img" src={getEmotionImage(emotionId)} />
+      <div className="emotion_name">{emotionName}</div>
+    </div>
+  );
 };
 
 export default EmotionItem;
